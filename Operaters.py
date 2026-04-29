4bd6dacc8a99f4883f43d145b2748d11aa5fec55
ramukotapally-png <ramukotapally@gmail.com>
2026-04-29 18:45:30 -0400

    I leanrt how to calculate the average and the percentage of numbers!

---
+++ Operaters.py
@@ -0,0 +1,13 @@
+maths = 80
+science = 80
+language = 90
+average = (maths + science + language)/3
+print(average)
+
+maths = int(input("Enter your maths score"))
+science = int(input("Enter your science score"))
+language = int(input("Enter your language score"))
+print(f"Your score in math is {maths}")
+print(f"Your score in science is {science}")
+print(f"Your score in language is {language}")
+maths + science + language
